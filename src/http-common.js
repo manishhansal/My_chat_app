d28d6b1945f31871bd70eb8100e2c2b0df39ab5b
Manish Kumar <manishhansalmeh@gmail.com>
2023-04-09 22:15:26 +0530

    updated backend url

--- src/http-common.js
+++ src/http-common.js
@@ -1,4 +1,5 @@
-export const BASEURL = "https://my-chat-app-backend.vercel.app";
+export const BASEURL = "https://my-chat-app-backend-fho0.onrender.com";
 
 // localhost ----> http://localhost:5001
 // vercel ----> https://my-chat-app-backend.vercel.app
+// render ----> https://my-chat-app-backend-fho0.onrender.com
