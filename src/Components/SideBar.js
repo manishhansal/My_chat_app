0c8147f3f6820ee4da8315c12aa00520d3f7f14f
Manish Kumar <manishhansalmeh@gmail.com>
2023-04-10 00:01:00 +0530

    Fixed the UI issue of home page and added scrollbar in sidebar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,92 +1,129 @@
-import React, { useContext, useEffect } from 'react'
-import { Col, ListGroup, Row } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux';
-import { AppContext } from '../context/appContext';
-import {addNotifications, resetNotifications} from '../features/userSlice';
-import './Sidebar.css';
-import { BASEURL } from '../http-common';
+import React, { useContext, useEffect } from "react";
+import { Col, ListGroup, Row } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { AppContext } from "../context/appContext";
+import { addNotifications, resetNotifications } from "../features/userSlice";
+import "./Sidebar.css";
+import { BASEURL } from "../http-common";
 
 const SideBar = () => {
-    const {socket, setMembers, members, setCurrentRoom, setRooms, privateMemberMsg, rooms, setPrivateMemberMsg, currentRoom} = useContext(AppContext);
-    const user = useSelector(state => state.user);
-    const dispatch = useDispatch()
+  const {
+    socket,
+    setMembers,
+    members,
+    setCurrentRoom,
+    setRooms,
+    privateMemberMsg,
+    rooms,
+    setPrivateMemberMsg,
+    currentRoom,
+  } = useContext(AppContext);
+  const user = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-    function joinRoom(room, isPublic = true) {
-      if(!user) {
-        return alert('Please login')
-      }
-      socket.emit('join-room', room, currentRoom);
-      setCurrentRoom(room);
+  function joinRoom(room, isPublic = true) {
+    if (!user) {
+      return alert("Please login");
+    }
+    socket.emit("join-room", room, currentRoom);
+    setCurrentRoom(room);
 
-      if(isPublic) {
-        setPrivateMemberMsg(null)
-      }
-
-      //dispatch for notification.
-      dispatch(resetNotifications(room));
+    if (isPublic) {
+      setPrivateMemberMsg(null);
     }
 
-    socket.off('notifications').on('notifications', (room) => {
-      if(currentRoom !== room) dispatch(addNotifications(room))
-    })
+    //dispatch for notification.
+    dispatch(resetNotifications(room));
+  }
 
-    useEffect(() => {
-      if(user) {
-        setCurrentRoom('general');
-        getRooms();
-        socket.emit('join-room', 'general');
-        socket.emit('new-user');
-      }
-    }, [])
-    socket.off('new-user').on('new-user', (payload) => {
-      console.log(payload)
+  socket.off("notifications").on("notifications", (room) => {
+    if (currentRoom !== room) dispatch(addNotifications(room));
+  });
 
-      setMembers(payload)
-    })
-
-    function getRooms() {
-      fetch(`${BASEURL}/rooms`).then((res) => res.json()).then((data) => setRooms(data))
+  useEffect(() => {
+    if (user) {
+      setCurrentRoom("general");
+      getRooms();
+      socket.emit("join-room", "general");
+      socket.emit("new-user");
     }
+  }, []);
+  socket.off("new-user").on("new-user", (payload) => {
+    console.log(payload);
 
-    function orderIds(id1, id2) {
-      if(id1 > id2) {
-        return id1 + "-" + id2;
-      }
-      else {
-        return id2 + "-" + id1;
-      }
+    setMembers(payload);
+  });
+
+  function getRooms() {
+    fetch(`${BASEURL}/rooms`)
+      .then((res) => res.json())
+      .then((data) => setRooms(data));
+  }
+
+  function orderIds(id1, id2) {
+    if (id1 > id2) {
+      return id1 + "-" + id2;
+    } else {
+      return id2 + "-" + id1;
     }
+  }
 
-    function handlePrivateMemberMsg(member) {
-      setPrivateMemberMsg(member);
-      const roomId = orderIds(user._id, member._id);
-      joinRoom(roomId, false);
-    }
+  function handlePrivateMemberMsg(member) {
+    setPrivateMemberMsg(member);
+    const roomId = orderIds(user._id, member._id);
+    joinRoom(roomId, false);
+  }
 
-    if(!user) {
-      return <></>
-    }
-
+  if (!user) {
+    return <></>;
+  }
 
   return (
     <>
-        <h2>Available Rooms</h2>
-        <ListGroup>
-            {rooms.map((room,idx) => (
-                  <ListGroup.Item key={idx} onClick={() => joinRoom(room)} active={room === currentRoom} style={{cursor: "pointer", display: "flex", justifyContent: "space-between"}}>
-                    {room} {currentRoom !== room && <span className='badge rounded-pill bg-primary'>
-                      {user.newMessages[room]}
-                    </span>}
-                  </ListGroup.Item>
-              ))}
-        </ListGroup>
-        <h2>Members</h2>
+      <h2>Available Rooms</h2>
+      <ListGroup>
+        {rooms.map((room, idx) => (
+          <ListGroup.Item
+            key={idx}
+            onClick={() => joinRoom(room)}
+            active={room === currentRoom}
+            style={{
+              cursor: "pointer",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            {room}{" "}
+            {currentRoom !== room && (
+              <span className="badge rounded-pill bg-primary">
+                {user.newMessages[room]}
+              </span>
+            )}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+      <h2>Members</h2>
+      <div className="members-container" id="custom-scrollbar">
         {members.map((member) => (
-          <ListGroup.Item key={member.id} style={{cursor: 'pointer'}} active={privateMemberMsg?._id === member?._id} onClick={() => handlePrivateMemberMsg(member)} disabled={member._id === user._id}>
+          <ListGroup.Item
+            key={member.id}
+            style={{ cursor: "pointer" }}
+            active={privateMemberMsg?._id === member?._id}
+            onClick={() => handlePrivateMemberMsg(member)}
+            disabled={member._id === user._id}
+          >
             <Row>
-              <Col xs={2} className='member-status'>
-                <img src={member.picture} className="member-status-img" alt='pic'/>
-                {member.status === "online" ? <i className='fas fa-circle sidebar-online-status'></i> : <i className='fas fa-circle sidebar-offline-status'></i>}
+              <Col xs={2} className="member-status">
+                <img
+                  src={member.picture}
+                  className="member-status-img"
+                  alt="pic"
+                />
+                {member.status === "online" ? (
+                  <i className="fas fa-circle sidebar-online-status"></i>
+                ) : (
+                  <i className="fas fa-circle sidebar-offline-status"></i>
+                )}
               </Col>
               <Col xs={9}>
                 {member.name}
@@ -94,13 +131,16 @@ const SideBar = () => {
                 {member.status === "offline" && " (Offline)"}
               </Col>
               <Col xs={1}>
-                <span className='badge rounded-pill bg-primary'>{user.newMessages[orderIds(member._id, user._id)]}</span>
+                <span className="badge rounded-pill bg-primary">
+                  {user.newMessages[orderIds(member._id, user._id)]}
+                </span>
               </Col>
             </Row>
           </ListGroup.Item>
         ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
